b06a5e5949c16df3c5d6de7a874cbb36f70c08bf
clux <analsandblaster@gmail.com>
2017-08-03 01:46:37 +0100

    less shell + fix debug

--- bench.sh
+++ bench.sh
@@ -9,24 +9,15 @@ build_all() {
 }
 
 run_all() {
-  echo "Rust"
-  time ./rustforest 305 295 300
-  echo "C++"
-  time ./cppforest 305 295 300
-  echo "C++ (llvm)"
-  time ./cppforestclang 305 295 300
-  echo "Go"
-  time ./goforest 305 295 300
-  echo "NodeJS"
-  time ./forest.js 305 295 300
-  echo "Elixir"
-  time ./forest.ex 305 295 300
-  echo "Python 3"
-  time ./forest.py 305 295 300
-  echo "Python (PYPY3)"
-  time pypy3 forest.py 305 295 300
-  echo "Haskell"
-  time ./ghcforest 305 295 300
+  echo "Rust" && time ./rustforest 305 295 300
+  echo "C++" && time ./cppforest 305 295 300
+  echo "C++ (llvm)" && time ./cppforestclang 305 295 300
+  echo "Go" && time ./goforest 305 295 300
+  echo "NodeJS" && time ./forest.js 305 295 300
+  echo "Elixir" && time ./forest.ex 305 295 300
+  echo "Python 3" && time ./forest.py 305 295 300
+  echo "Python (PYPY3)" && time pypy3 forest.py 305 295 300
+  echo "Haskell" && time ./ghcforest 305 295 300
 }
 
 main() {
--- test.sh
+++ test.sh
@@ -8,23 +8,13 @@ build_all() {
 }
 
 verify_output() {
-  local -r noderes=$(./forest.js 105 95 100 | wc -l)
-  local -r pyres=$(./forest.py 105 95 100 | wc -l)
-  local -r gores=$(./goforest 105 95 100 | wc -l)
-  local -r cppres=$(./cppforest 105 95 100 | wc -l)
-  # haskell's traceShowId prints to stderr
-  local -r ghcres=$(./ghcforest 105 95 100 2>&1 | wc -l)
-  local -r iexres=$(./forest.ex 105 95 100 | wc -l)
-  local -r rustres=$(./rustforest 105 95 100 | wc -l)
-
-
-  [[ $noderes == 200216 ]]
-  [[ $pyres == 200216 ]]
-  [[ $gores == 200216 ]]
-  [[ $ghcres == 200216 ]]
-  [[ $iexres == 200216 ]]
-  [[ $rustres == 200216 ]]
-  [[ $cppres == 200216 ]]
+  [[ $(./forest.js 105 95 100 | wc -l) == 200216 ]]
+  [[ $(./forest.py 105 95 100 | wc -l) ]]
+  [[ $(./goforest 105 95 100 | wc -l) == 200216 ]]
+  [[ $(./ghcforest 105 95 100 2>&1 | wc -l) == 200216 ]]
+  [[ $(./forest.ex 105 95 100 | wc -l) == 200216 ]]
+  [[ $(./rustforest 105 95 100 | wc -l) == 200216 ]]
+  [[ $(./cppforest 105 95 100 | wc -l) == 200216 ]]
 }
 
 main() {
