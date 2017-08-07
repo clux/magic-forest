04a7f053b2585768068dce448a4c6b2cb211f489
John Children <john@cognitivelogic.com>
2017-08-07 21:28:56 +0100

    Adds a naive fortran implementation

--- test.sh
+++ test.sh
@@ -5,9 +5,11 @@ build_all() {
   ghc -O forest.hs -o ghcforest # creates forest.{hi,o} pointlessly
   g++ -O3 -std=c++14 forest.cpp -o cppforest
   rustc -C opt-level=3 forest.rs -o rustforest
+  gfortran -O3 forest.f08 -o fortranforest
 }
 
 verify_output() {
+  [[ $(./fortranforest 105 95 100 | wc -l) == 200218 ]]
   [[ $(./forest.js 105 95 100 | wc -l) == 200216 ]]
   [[ $(./forest.py 105 95 100 | wc -l) ]]
   [[ $(./goforest 105 95 100 | wc -l) == 200216 ]]
