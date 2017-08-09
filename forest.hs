8e36a354212078e2ba26ebdd4cdc7b243577b752
John Children <john.a.children@gmail.com>
2017-08-09 16:54:43 +0100

    Adds non-cheating haskell implementation with native lists

--- forest.hs
+++ forest.hs
@@ -1,41 +1,53 @@
 module Main where
 
-import qualified Data.Set as S
-import System.Environment
+import           Data.List
+import           System.Environment
 
 data Forest = Forest
-  { goats :: !Int
+  { goats  :: !Int
   , wolves :: !Int
-  , lions :: !Int
-} deriving (Show, Ord, Eq)
+  , lions  :: !Int
+  } deriving (Show, Ord, Eq)
 
 isStable :: Forest -> Bool
 isStable (Forest 0 0 _) = True
 isStable (Forest 0 _ 0) = True
 isStable (Forest _ 0 0) = True
-isStable _ = False
+isStable _              = False
 
 isValid :: Forest -> Bool
 isValid (Forest g w l) = g >= 0 && w >= 0 && l >= 0
 
-mutate :: S.Set Forest -> S.Set Forest
-mutate = S.filter isValid . S.unions . f
-  where f set = [S.map lionEats set, S.map wolfEats set, S.map goatEats set]
-        lionEats (Forest g w l) = Forest (g-1) (w-1) (l+1)
-        wolfEats (Forest g w l) = Forest (g-1) (w+1) (l-1)
-        goatEats (Forest g w l) = Forest (g+1) (w-1) (l-1)
-
-solve :: Forest -> S.Set Forest
-solve = solve' . S.singleton
+-- removes adjacent identical elements
+dedup :: [Forest] -> [Forest]
+dedup = go $ Forest 0 0 0
   where
-  solve' xs | cond xs = solve' $ mutate xs
-            | otherwise = S.filter isStable xs
-  cond vec | S.null vec = False
-           | otherwise = S.null . S.filter isStable $ vec
+    go s (x:xs)
+      | x == s = go s xs
+      | otherwise = x : go x xs
+    go _ _ = []
+
+mutate :: [Forest] -> [Forest]
+mutate xs = dedup . sort . filter isValid . concatMap f $ xs
+  where
+    f (Forest g w l) =
+      [ Forest (g - 1) (w - 1) (l + 1)
+      , Forest (g - 1) (w + 1) (l - 1)
+      , Forest (g + 1) (w - 1) (l - 1)
+      ]
+
+solve :: Forest -> [Forest]
+solve x = solve' [x]
+  where
+    solve' xs
+      | cond xs = solve' $ mutate xs
+      | otherwise = filter isStable xs
+    cond [] = False
+    cond xs = null . filter isStable $ xs
 
 main :: IO ()
 main = do
   [g, w, l] <- fmap (map read) $ getArgs
-  let initial = (Forest g w l)
+  let initial = Forest g w l
   print initial
   print $ solve initial
