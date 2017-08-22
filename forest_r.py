9157faa9da3b5cececec0d4bd5b31e848524d46f
Ben Cordero <bencord0@condi.me>
2017-08-22 18:08:08 +0100

    rpython: Use tuples, there's no other way
    - Can't subclass object, no custom `__hash__` functions
    - Can't subclass tuple, it's a builtin and has other funnyness
      around `getuniqueclassdef`.

--- forest_r.py
+++ forest_r.py
@@ -1,40 +1,17 @@
-class Forest(object):
-    def __init__(self, goats, wolves, lions):
-        self.goats = goats
-        self.wolves = wolves
-        self.lions = lions
-
-    def __eq__(self, other):
-        return (
-            self.goats == other.goats and
-            self.wolves == other.wolves and
-            self.lions == other.lions
-        )
-
-    def __hash__(self):
-        return hash((self.goats, self.wolves, self.lions))
-
-    def __str__(self):
-        return "%d %d %d" % (self.goats, self.wolves, self.lions)
-
-    def __repr__(self):
-        return str(self)
-
-
 def is_stable(f):
-    return (f.goats == 0 and (f.wolves == 0 or f.lions == 0)) or (f.wolves == 0 and f.lions == 0)
+    return (f[0] == 0 and (f[1] == 0 or f[2] == 0)) or (f[1] == 0 and f[2] == 0)
 
 
 def is_valid(f):
-    return f.goats >= 0 and f.wolves >= 0 and f.lions >= 0
+    return f[0] >= 0 and f[1] >= 0 and f[2] >= 0
 
 
 def mutate(forests):
     next_forests = []
     for f in forests:
-        next_forests.append(Forest(f.goats - 1, f.wolves - 1, f.lions + 1))
-        next_forests.append(Forest(f.goats - 1, f.wolves + 1, f.lions - 1))
-        next_forests.append(Forest(f.goats + 1, f.wolves - 1, f.lions - 1))
+        next_forests.append((f[0] - 1, f[1] - 1, f[2] + 1))
+        next_forests.append((f[0] - 1, f[1] + 1, f[2] - 1))
+        next_forests.append((f[0] + 1, f[1] - 1, f[2] - 1))
 
     # rpython has no `set()`
     forest_map = {}
@@ -63,20 +40,19 @@ def solve(forest):
     forests = [forest]
     while any(forests) and not has_stable_forest(forests):
         forests = mutate(forests)
-    print('forests: %d' % len(forests))
     return [f for f in forests if is_stable(f)]
 
 
 def entry_point(args):
     if len(args) != 4:
-        print('USAGE: ' + args[0] + ' <goats> <wolves> <lions>')
+        print 'USAGE: ' + args[0] + ' <goats> <wolves> <lions>'
         return 1
 
-    forest = Forest(int(args[1]), int(args[2]), int(args[3]))
-    print('Initial:', forest)
+    forest = (int(args[1]), int(args[2]), int(args[3]))
+    print 'Initial:', forest
 
     for f in solve(forest):
-        print('Solution:', f)
+        print 'Solution:', f
     return 0
 
 
