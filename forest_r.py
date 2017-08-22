58b7b3b4a4e48b943174fcb33de57cf8df1d6cae
Ben Cordero <bencord0@condi.me>
2017-08-22 07:05:04 +0100

    Add RPython implementation
    What is RPython?
    ----------------
    
    "RPython is a restricted subset of Python2 that is amenable to static
    analysis" - rpython.readthedocs.io/en/latest/rpython.html
    
    No, you really shouldn't be writing programs in it. It's really
    designed for writing language interpreters.
    
    It is distributed as part of the pypy_ project.
    
    .. _pypy: http://pypy.org/
    
    What is this?
    -------------
    
    An implementation of `forest.py`, adapted to the RPython toolchain
    
    Changes needed:
    
      - Add `entry_point` (where the compiled binary starts)
      - Add `target` (where the rpython compiler starts)
      - Add compatibility `__name__ == __main__` (where python can start)
      - No use of the standard library, not even `sys`. Namedtuples have
        been replaced with tuples (this might be a rule breaker).
      - Reduced string formatting options, you basically have str/str
        concat, and C-style formatting (if you need to convert ints).
      - RPython has no implementation of `any` or `set`.
      - RPython does have generators, but they're not very complex.
      - Watch out for functions that return multiple types.
        e.g. avoid returning both lists and iterators, as they don't
        share a subclass (and thus are not allowed). It's probably
        more efficient in RPython to just use preallocated lists.
    
    How do I run it?
    ----------------
    
    RPython programs are valid Python programs. You could just run
    this under a normal python interpreter. Since the language is
    simpler for an interpreter to reason with, you might get a boost
    in performance anyway. The tradeoff is that an RPython implementation
    is no longer an idiomatic Python implementation.
    I'm not even sure if "idiomatic RPython" is a phraise that should exist.
    
    How do I compile it?
    --------------------
    
    You will need a copy of the RPython toolchain. It is implemented
    in python, so you don't need much more than to clone the pypy source.
    
        $ hg clone https://bitbucket.org/pypy/pypy ../pypy
    
    The rpython toolchain in in the `rpython` subdirectory.
    
        $ ../pypy/rpython/bin/rpython ./forest_r.py
    
    This will output `forest_r-c`.
    
        $ time ./forest_r-c 305 295 300

---
+++ forest_r.py
@@ -0,0 +1,61 @@
+def is_stable(f):
+    return (f[0] == 0 and (f[1] == 0 or f[2] == 0)) or (f[1] == 0 and f[2] == 0)
+
+
+def is_valid(f):
+    return f[0] >= 0 and f[1] >= 0 and f[2] >= 0
+
+
+def mutate(forests):
+    next_forests = []
+    for f in forests:
+        next_forests.append((f[0] - 1, f[1] - 1, f[2] + 1))
+        next_forests.append((f[0] - 1, f[1] + 1, f[2] - 1))
+        next_forests.append((f[0] + 1, f[1] - 1, f[2] - 1))
+
+    # rpython has no `set()`
+    forest_map = {}
+    for f in next_forests:
+        forest_map[f] = None
+    return list(forest_map.keys())
+ 
+
+def any(iterable):
+    for i in iterable:
+        if i:
+            return True
+    return False
+
+
+def solve(forest):
+    # rpython can't use complex generators
+    # e.g. avoid running multiple generators at the same time
+    forests = [forest]
+    stable_forests = [f for f in forests if is_stable(f)]
+    while any(forests) and not any(stable_forests):
+        forests = mutate(forests)
+        stable_forests = [f for f in forests if is_stable(f)]
+    return stable_forests
+
+
+def entry_point(args):
+    if len(args) != 4:
+        print('USAGE: ' + args[0] + ' <goats> <wolves> <lions>')
+        return 1
+
+    initial = (int(args[1]), int(args[2]), int(args[3]))
+    print('Initial: %d %d %d' % initial)
+
+    for f in solve(initial):
+        print('Solution: %d %d %d' % initial)
+    return 0
+
+
+def target(*args):
+    return entry_point, None
+
+
+# Compatability with python interpreters
+if __name__ == '__main__':
+    import sys
+    sys.exit(entry_point(sys.argv))
